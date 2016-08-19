125c4a4ed27fd841878f7c491e63e1d831799b33
Mezcel <mezcel@hotmail.com>
2016-08-19 16:23:09 -0400

    Client Service Interface

---
+++ IClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChattingInterfaces
+{   
+
+    public interface IClient
+    {
+        [OperationContract]
+        void GetMessage(string message, string userName);
+
+        [OperationContract]
+        void GetUpdate(int value, string userName);
+
+        #region Bead
+        
+        [OperationContract]
+        void GetBeadMessage(string message, string userName);
+
+        [OperationContract]
+        void GetBeadNumberMessage(int message, string userName);
+
+        [OperationContract]
+        void GetBeadListMessage(List<string> listofbeadpeople, string userName);
+
+        #endregion Bead
+    }
+}
